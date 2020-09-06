89243249f2a760374eb2bca9cd5593a04375a457
joorgeroberto <jorgercj@dcomp.ufs.br>
2020-09-06 19:14:17 -0300

    (refactor): Melhorando o código

--- ModelagemSupermercado.playground/Contents.swift
+++ ModelagemSupermercado.playground/Contents.swift
@@ -2,6 +2,7 @@ import UIKit
 
 //Classe pai
 class Produto {
+    //Atributos originais
     private var nome: String
     private var fornecedor: String
     private var quantidade: Double
@@ -9,15 +10,27 @@ class Produto {
     private var dataDeFabricacao: Date
     private var descricao: String
     
-    init(nome: String, fornecedor: String, quantidade: Double, preco: Float, dataDeFabricacao: Date, descricao: String) {
+    //Atributos dos filhos trazidos para o pai
+    private var fabricante: String
+    private var tipo: String
+    private var id: Double
+    private var dataDeVencimento: Date
+    
+    init(nome: String, fornecedor: String, quantidade: Double, preco: Float, dataDeFabricacao: Date, descricao: String, fabricante: String, tipo: String, id: Double, dataDeVencimento: Date) {
         self.nome = nome
         self.fornecedor = fornecedor
         self.quantidade = quantidade
         self.preco = preco
         self.dataDeFabricacao = dataDeFabricacao
         self.descricao = descricao
+        
+        self.fabricante = fabricante
+        self.tipo = tipo
+        self.id = id
+        self.dataDeVencimento = dataDeVencimento
     }
     
+    //Métodos iniciais
     func getNome() -> String {
         return self.nome;
     }
@@ -65,7 +78,7 @@ class Produto {
     }
     
     func getInfo() -> String {
-        return ("Nome: \(nome) \nFornecedor: \(fornecedor) \nData de Fabricação: \(dataDeFabricacao) \nQuantidade: \(quantidade) \nPreço: \(preco) \nDescrição: \(descricao)\n")
+        return ("Nome: \(nome) \nFornecedor: \(fornecedor) \nData de Fabricação: \(dataDeFabricacao) \nQuantidade: \(quantidade) \nPreço: \(preco) \nDescrição: \(descricao)\n Fabricante: \(fabricante)\n Tipo: \(tipo)\n Id: \(id)\n Data de Vencimento: \(dataDeVencimento)")
     }
     
     func comprar(_ numeroDeUnidadesCompradas: Double) {
@@ -77,27 +90,8 @@ class Produto {
             print("\(numeroDeUnidadesCompradas) unidades de \(self.nome) não disponível no estoque. Atualmente existem \(self.quantidade) unidades disponíveis.")
         }
     }
-}
-
-class Bebida: Produto {
-    private var fabricante: String
-    private var tipo: String
-    private var volumeDaUnidade: String
-    private var id: Double
-    private var dataDeVencimento: Date
-    private var gelada: Bool
     
-    init(nome: String, fornecedor: String, quantidade: Double, preco: Float, dataDeFabricacao: Date, descricao: String, fabricante: String, tipo: String, volumeDaUnidade: String, id: Double, dataDeVencimento: Date, gelada: Bool) {
-        self.fabricante = fabricante
-        self.tipo = tipo
-        self.volumeDaUnidade = volumeDaUnidade
-        self.id = id
-        self.dataDeVencimento = dataDeVencimento
-        self.gelada = gelada
-        
-        super.init(nome: nome, fornecedor: fornecedor, quantidade: quantidade, preco: preco, dataDeFabricacao: dataDeFabricacao, descricao: descricao)
-    }
-    
+    //Métodos dos filhos:
     func getFabricante() -> String {
         return self.fabricante;
     }
@@ -106,19 +100,12 @@ class Bebida: Produto {
         return self.tipo;
     }
     
-    func getVolumeDaUnidade() -> String {
-        return self.volumeDaUnidade;
-    }
-    
     func getId() -> Double {
         self.id;
     }
     
     func getDataDeVencimento() -> Date {
         return self.dataDeVencimento;
-    }
-    func getGelada() -> Bool {
-        return self.gelada;
     }
     
     func setFabricante(fabricante: String) {
@@ -129,10 +116,6 @@ class Bebida: Produto {
         return self.tipo = tipo;
     }
     
-    func setVolumeDaUnidade(_ volumeDaUnidade:String) {
-        return self.volumeDaUnidade = volumeDaUnidade;
-    }
-    
     func setId(_ id: Double) {
         self.id = id;
     }
@@ -140,90 +123,82 @@ class Bebida: Produto {
     func setDataDeVencimento(_ dataDeVencimento: Date) {
         return self.dataDeVencimento = dataDeVencimento;
     }
+}
+
+class Bebida: Produto {
+    private var volumeDaUnidade: String
+    private var gelada: Bool
+    
+    init(nome: String, fornecedor: String, quantidade: Double, preco: Float, dataDeFabricacao: Date, descricao: String, fabricante: String, tipo: String, volumeDaUnidade: String, id: Double, dataDeVencimento: Date, gelada: Bool) {
+        self.volumeDaUnidade = volumeDaUnidade
+        self.gelada = gelada
+        
+        super.init(nome: nome, fornecedor: fornecedor, quantidade: quantidade, preco: preco, dataDeFabricacao: dataDeFabricacao, descricao: descricao, fabricante: fabricante, tipo: tipo, id: id, dataDeVencimento: dataDeVencimento)
+    }
+    
+    
+    
+    func getVolumeDaUnidade() -> String {
+        return self.volumeDaUnidade;
+    }
+    
+    
+    func getGelada() -> Bool {
+        return self.gelada;
+    }
+    
+    
+    func setVolumeDaUnidade(_ volumeDaUnidade:String) {
+        return self.volumeDaUnidade = volumeDaUnidade;
+    }
+    
+    
     func setGelada(_ gelada: Bool) {
         return self.gelada = gelada;
     }
     
     override func getInfo() -> String {
-        return (super.getInfo() + "Fabricante: \(fabricante) \nTipo: \(tipo) \nVolume da Unidade: \(volumeDaUnidade)\nId: \(id) \nData de Vencimento: \(dataDeVencimento) \nGelada: \(gelada)");
+        return (super.getInfo() + "\nVolume da Unidade: \(volumeDaUnidade)\n Gelada: \(gelada)");
     }
 }
 
 class Comida: Produto {
-    private var fabricante: String
-    private var tipo: String
     private var tipoDeUnidade: String
-    private var id: Double
-    private var dataDeVencimento: Date
     private var congelada: Bool
     
     init(nome: String, fornecedor: String, quantidade: Double, preco: Float, dataDeFabricacao: Date, descricao: String, fabricante: String, tipo: String, tipoDeUnidade: String, id: Double, dataDeVencimento: Date, congelada: Bool) {
-        self.fabricante = fabricante
-        self.tipo = tipo
         self.tipoDeUnidade = tipoDeUnidade
-        self.id = id
-        self.dataDeVencimento = dataDeVencimento
         self.congelada = congelada
         
-        super.init(nome: nome, fornecedor: fornecedor, quantidade: quantidade, preco: preco, dataDeFabricacao: dataDeFabricacao, descricao: descricao)
-    }
-    
-    func getFabricante() -> String {
-        return self.fabricante;
-    }
-    
-    func getTipo() -> String {
-        return self.tipo;
+        super.init(nome: nome, fornecedor: fornecedor, quantidade: quantidade, preco: preco, dataDeFabricacao: dataDeFabricacao, descricao: descricao, fabricante: fabricante, tipo: tipo, id: id, dataDeVencimento: dataDeVencimento)
     }
     
     func getTipoDeUnidade() -> String {
         return self.tipoDeUnidade;
     }
     
-    func getId() -> Double {
-        self.id;
-    }
-    
-    func getDataDeVencimento() -> Date {
-        return self.dataDeVencimento;
-    }
     func getCongelada() -> Bool {
         return self.congelada;
     }
     
-    func setFabricante(fabricante: String) {
-        return self.fabricante = fabricante;
-    }
-    
-    func setTipo (tipo: String) {
-        return self.tipo = tipo;
-    }
     
     func setTipoDeUnidade(tipoDeUnidade: String) {
         return self.tipoDeUnidade = tipoDeUnidade;
-    }
-    
-    func setId(id: Double) {
-        self.id = id;
-    }
-    
-    func setDataDeVencimento(dataDeVencimento: Date) {
-        return self.dataDeVencimento = dataDeVencimento;
     }
     func setCongelada(congelada: Bool) {
         return self.congelada = congelada;
     }
     
     override func getInfo() -> String {
-        return (super.getInfo() + "Fabricante: \(fabricante) \nTipo: \(tipo) \nTipo de Unidade: \(tipoDeUnidade) \nId: \(id) \nData de Vencimento: \(dataDeVencimento) \nCongelada: \(congelada)")
+        return (super.getInfo() + "Tipo de Unidade: \(tipoDeUnidade)\n Congelada: \(congelada)")
     }
 }
 
-let primeiroProduto = Produto(nome: "Produto 1", fornecedor: "Fornecedor 1", quantidade: 001, preco: 1.888, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto");
+let primeiroProduto = Produto(nome: "Produto 1", fornecedor: "Fornecedor 1", quantidade: 001, preco: 30, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto", fabricante: "Fabricante 3", tipo: "Fruta", id: 001, dataDeVencimento: Date());
 
-let segundoProduto = Bebida(nome: "Produto 2", fornecedor: "Fornecedor 2", quantidade: 002, preco: 18.88, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto", fabricante: "fabricante 1", tipo: "refrigerante", volumeDaUnidade: "ml", id: 123, dataDeVencimento: Date(), gelada: true);
+let segundoProduto = Bebida(nome: "Produto 2", fornecedor: "Fornecedor 2", quantidade: 002, preco: 18.88, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto", fabricante: "fabricante 1", tipo: "Refrigerante", volumeDaUnidade: "ml", id: 010, dataDeVencimento: Date(), gelada: true);
 
-let terceiroProduto = Comida(nome: "Produto 3", fornecedor: "Fornecedor 3", quantidade: 002, preco: 10, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto", fabricante: "Fabricante 2", tipo: "Carne", tipoDeUnidade: "kg", id: 123, dataDeVencimento: Date(), congelada: true);
+let terceiroProduto = Comida(nome: "Produto 3", fornecedor: "Fornecedor 3", quantidade: 002, preco: 10, dataDeFabricacao: Date(), descricao: "Este é um ótimo produto", fabricante: "Fabricante 2", tipo: "Carne", tipoDeUnidade: "kg", id: 011, dataDeVencimento: Date(), congelada: true);
 
 print("\(primeiroProduto.getInfo())\n")
 print("\(segundoProduto.getInfo())\n")
---
+++ README.md
@@ -0,0 +1,26 @@
+# Supermercado
+## Digital House - Turma 01 - IOS (Swift)
+
+Para praticar conceitos de POO, foi proposta a modelagem de um aplicativo de supermercado usando a Linguagem Swift no Playground do Xcode.
+
+- Requisitos Mínimos:
+  * 3 classes;
+  * 6 atributos para cada classe;
+  * 5 métodos;
+  * 1 aplicação de Herança;
+  
+<br>
+
+### Versão 1:
+Inicialmente, foi criado o diagrama de classes para representar os produtos do aplicativo: <br> 
+<img alt="calculator_preview" width="600" src="https://github.com/joorgeroberto/modelagemSupermercado/blob/master/Images/Supermercado_inicial.png">
+
+O código desenvolvido se encontra neste link: https://github.com/joorgeroberto/modelagemSupermercado/blob/dbd093d028d7549ec8eeb1324c9b785caa1e149e/ModelagemSupermercado.playground/Contents.swift
+
+:white_check_mark: Como podemos ver, os requisitos mínimos foram atingidos :white_check_mark:
+<br>
+### Melhorias:
+Como a atividade visa a prática de conceitos simples de POO como herança e uso de funções, a modelagem não ficou muito otimizada. Algumas melhorias podem ser feitas, por exemplo, em atributos que se repetem em classes filhas e poderiam ser atribuidos à classe pai, diminuindo, assim, a quantidade e repetição de código.
+
+Este novo diagrama melhorado ficaria assim: <br> 
+<img alt="calculator_preview" width="600" src="https://github.com/joorgeroberto/modelagemSupermercado/blob/master/Images/Supermercado_v2.png">
